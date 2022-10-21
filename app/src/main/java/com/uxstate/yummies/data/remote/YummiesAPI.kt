6220b48cc49b13c9719c3980e822b7b1db31c2e8
Tonnie <vontonnie@gmail.com>
2022-10-21 16:05:27 +0300

    Add getMealsByid() YummiesAPI interface function

--- app/src/main/java/com/uxstate/yummies/data/remote/YummiesAPI.kt
+++ app/src/main/java/com/uxstate/yummies/data/remote/YummiesAPI.kt
@@ -7,12 +7,19 @@ import retrofit2.http.Query
 
 interface YummiesAPI {
 
-    //Search meal by name
+    //Search meal by name - www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+
     @GET("search.php")
-    suspend fun getMealsByName(@Query("s") query: String): MealsResponseDTO
+    suspend fun getMealsByName(@Query("s") name: String): MealsResponseDTO
+
+    //Lookup full meal details by id - www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+
+    @GET("lookup.php")
+    suspend fun getMealsById(@Query("i") id:String):MealsResponseDTO
 
 
-    //List all meal categories
+    //List all meal categories - www.themealdb.com/api/json/v1/1/categories.php
+
     @GET("categories.php")
     suspend fun getCategories():CategoriesResponseDTO
 
