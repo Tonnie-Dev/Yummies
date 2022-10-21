d34b975ac6683763c6e5e15008c141d2366496c1
Tonnie <vontonnie@gmail.com>
2022-10-21 15:59:32 +0300

    Add getCategories() YummiesAPI interface function

--- app/src/main/java/com/uxstate/yummies/data/remote/YummiesAPI.kt
+++ app/src/main/java/com/uxstate/yummies/data/remote/YummiesAPI.kt
@@ -1,12 +1,20 @@
 package com.uxstate.yummies.data.remote
 
+import com.uxstate.yummies.data.remote.dto.CategoriesResponseDTO
 import com.uxstate.yummies.data.remote.dto.MealsResponseDTO
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface YummiesAPI {
+
+    //Search meal by name
     @GET("search.php")
     suspend fun getMealsByName(@Query("s") query: String): MealsResponseDTO
+
+
+    //List all meal categories
+    @GET("categories.php")
+    suspend fun getCategories():CategoriesResponseDTO
 
     companion object {
 
