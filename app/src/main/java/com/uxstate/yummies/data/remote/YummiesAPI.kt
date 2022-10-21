bb4868fe42a2c67eda8d80f6527f3eb28cb64df9
Tonnie <vontonnie@gmail.com>
2022-10-21 15:53:16 +0300

    Add teh Base URL to YummiesAPI companion object

--- app/src/main/java/com/uxstate/yummies/data/remote/YummiesAPI.kt
+++ app/src/main/java/com/uxstate/yummies/data/remote/YummiesAPI.kt
@@ -5,4 +5,9 @@ import com.uxstate.yummies.data.remote.dto.MealsResponseDTO
 interface YummiesAPI {
 
     suspend fun getMealsByName():MealsResponseDTO
+
+    companion object{
+
+        const val BASE_URL = "www.themealdb.com/api/json/v1/1/"
+    }
 }
