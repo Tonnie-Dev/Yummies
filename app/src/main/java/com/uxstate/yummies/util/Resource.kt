7516ef0461de2369794c4c128fda6cec6e5397c8
Tonnie <vontonnie@gmail.com>
2022-10-21 20:47:23 +0300

    Add Resource sealed class in the util package

---
+++ app/src/main/java/com/uxstate/yummies/util/Resource.kt
@@ -0,0 +1,13 @@
+package com.uxstate.yummies.util
+
+sealed class Resource<T>(val data: T? = null, val errorMessage: String? = null) {
+
+    //Error - attach some data in case we need to show stale data
+    class Error<T>(data: T?, errorMessage: String) : Resource<T>(data, errorMessage)
+    
+    //Success
+    class Success<T>(data: T?) : Resource<T>(data = data)
+
+    //Loading
+    class Loading<T>(val loading: Boolean = true) : Resource<T>()
+}
