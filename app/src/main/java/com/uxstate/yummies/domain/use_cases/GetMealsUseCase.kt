bd075533c59f3d5d7c1e4e14d5c81759bc1b13ff
Tonnie <vontonnie@gmail.com>
2022-10-22 18:30:30 +0300

    Add GetmealsUseCase class

---
+++ app/src/main/java/com/uxstate/yummies/domain/use_cases/GetMealsUseCase.kt
@@ -0,0 +1,14 @@
+package com.uxstate.yummies.domain.use_cases
+
+import com.uxstate.yummies.domain.model.Meal
+import com.uxstate.yummies.domain.repository.YummiesRepository
+import com.uxstate.yummies.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+class GetMealsUseCase(private val repository: YummiesRepository) {
+
+    operator fun invoke(query: String, fetchFromRemote: Boolean): Flow<Resource<List<Meal>>> {
+
+        return repository.fetchMealItems(query = query, fetchFromRemote = fetchFromRemote)
+    }
+}
