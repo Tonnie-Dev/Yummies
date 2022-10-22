9335af146ef35b86c847ecd1d1db35a010274bfd
Tonnie <vontonnie@gmail.com>
2022-10-22 10:29:52 +0300

    Add MealEntity class

---
+++ app/src/main/java/com/uxstate/yummies/data/local/entity/MealEntity.kt
@@ -0,0 +1,18 @@
+package com.uxstate.yummies.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "meals_table")
+data class MealEntity(
+
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val name: String,
+    val category: String,
+    val origin: String,
+    val directions: String,
+    val imageUrl: String,
+    val ingredients: List<String>,
+    val units: List<String>
+)
