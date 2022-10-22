3837f35c14451c71faf7e07f9dff59befc0f9244
Tonnie <vontonnie@gmail.com>
2022-10-22 11:21:45 +0300

    Add insertCategories() dao function

--- app/src/main/java/com/uxstate/yummies/data/local/YummiesDAO.kt
+++ app/src/main/java/com/uxstate/yummies/data/local/YummiesDAO.kt
@@ -3,6 +3,7 @@ package com.uxstate.yummies.data.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import com.uxstate.yummies.data.local.entity.CategoryEntity
 import com.uxstate.yummies.data.local.entity.MealEntity
 
 
@@ -10,5 +11,8 @@ import com.uxstate.yummies.data.local.entity.MealEntity
 interface YummiesDAO{
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMeals(meals:List<MealEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCategories(categories:List<CategoryEntity>)
 
 }
