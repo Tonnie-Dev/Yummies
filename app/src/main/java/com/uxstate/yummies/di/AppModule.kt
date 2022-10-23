c4ab6fb878fdec336132156f58e937d7fe90cc1e
Tonnie <vontonnie@gmail.com>
2022-10-23 17:12:49 +0300

    Add AppModule object

--- app/src/main/java/com/uxstate/yummies/di/AppModule.kt
+++ app/src/main/java/com/uxstate/yummies/di/AppModule.kt
@@ -1,8 +1,13 @@
 package com.uxstate.yummies.di
 
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
 
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+    
+
 }
