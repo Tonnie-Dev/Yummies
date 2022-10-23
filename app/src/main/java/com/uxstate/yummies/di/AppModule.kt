5c11f650f40525ae567996757ea41918348730eb
Tonnie <vontonnie@gmail.com>
2022-10-23 17:25:21 +0300

    Add provideApiService() di function

--- app/src/main/java/com/uxstate/yummies/di/AppModule.kt
+++ app/src/main/java/com/uxstate/yummies/di/AppModule.kt
@@ -2,20 +2,26 @@ package com.uxstate.yummies.di
 
 import android.content.Context
 import androidx.room.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.uxstate.yummies.data.local.YummiesDatabase
+import com.uxstate.yummies.data.remote.YummiesAPI
 import com.uxstate.yummies.util.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-// provide database
+    // provide database
 
     @Provides
     @Singleton
@@ -23,9 +29,27 @@ object AppModule {
     fun provideDatabase(@ApplicationContext context: Context): YummiesDatabase {
 
         return Room.databaseBuilder(
-            context,
-            YummiesDatabase::class.java,
-            Constants.DATABASE_NAME
-        ).build()
+                context,
+                YummiesDatabase::class.java,
+                Constants.DATABASE_NAME
+        )
+                .build()
+    }
+
+    @Provides
+    @Singleton
+
+    fun provideApiService(): YummiesAPI {
+
+
+        val moshi = Moshi.Builder()
+                .addLast(KotlinJsonAdapterFactory())
+                .build()
+        
+        return Retrofit.Builder()
+                .baseUrl(YummiesAPI.BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+                .create()
     }
 }
