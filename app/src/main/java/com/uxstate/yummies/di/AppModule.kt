02ef7e24295193efd6c2442e1f68548e28eb495e
Tonnie <vontonnie@gmail.com>
2022-10-23 17:17:08 +0300

    Add Constants object in the util package

--- app/src/main/java/com/uxstate/yummies/di/AppModule.kt
+++ app/src/main/java/com/uxstate/yummies/di/AppModule.kt
@@ -1,13 +1,32 @@
 package com.uxstate.yummies.di
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.uxstate.yummies.data.local.YummiesDatabase
+import com.uxstate.yummies.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    
 
+//provide database
+
+
+
+    @Provides
+    @Singleton
+
+    fun provideDatabase(@ApplicationContext context:Context):YummiesDatabase {
+
+
+        return Room.databaseBuilder(context,YummiesDatabase::class.java, Constants.DATABASE_NAME).build()
+    }
 }
---
+++ app/src/main/java/com/uxstate/yummies/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.uxstate.yummies.util
+
+object Constants {
+
+    const val DATABASE_NAME = "yummies_database"
+}
