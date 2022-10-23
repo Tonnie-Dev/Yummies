968e933efa955717ca3be302f8db74d26217acdd
Tonnie <vontonnie@gmail.com>
2022-10-23 22:40:45 +0300

    Add getMeals() viewModel function

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/OverviewViewModel.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/OverviewViewModel.kt
@@ -1,64 +1,102 @@
 package com.uxstate.yummies.presentation.screens.overview_screen
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.uxstate.yummies.domain.model.Meal
 import com.uxstate.yummies.domain.use_cases.UseCaseContainer
 import com.uxstate.yummies.presentation.screens.overview_screen.states.StateCategories
+import com.uxstate.yummies.presentation.screens.overview_screen.states.StateMeals
 import com.uxstate.yummies.util.Resource
 import com.uxstate.yummies.util.UiEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class OverviewViewModel @Inject constructor(private val container: UseCaseContainer) : ViewModel() {
 
-    val query by mutableStateOf("")
-    val fetchFromRemote by mutableStateOf(false)
 
     private val _stateCategories = MutableStateFlow(StateCategories())
     val stateCategory = _stateCategories.asStateFlow()
 
-    private val _meals = MutableStateFlow<List<Meal>>(emptyList())
-    val meals = _meals.asStateFlow()
+    private val _stateMeals = MutableStateFlow(StateMeals())
+    val stateMeals = _stateMeals.asStateFlow()
 
     private val _uiEvent = MutableSharedFlow<UiEvent>()
     val uiEvent = _uiEvent.asSharedFlow()
 
+    val searchJob: Job? = null
 
+
+    private fun getMeals(query: String = "", fetchFromRemote: Boolean = false) {
+
+        viewModelScope.launch {
+
+            container.getMealsUseCase(
+                    query = query,
+                    fetchFromRemote = fetchFromRemote
+            )
+                    .collectLatest {
+
+                        result ->
+
+                        when (result) {
+
+                            is Resource.Success -> {
+
+                                result.data?.let {
+
+                                    _stateMeals.value = StateMeals().copy(meals = it)
+                                }
+                            }
+                            is Resource.Error -> {
+
+                                _uiEvent.emit(
+                                        UiEvent.ShowSnackbar(
+                                                result.errorMessage ?: "Unknown Error"
+                                        )
+                                )
+                            }
+                            is Resource.Loading -> {
+
+                                _stateMeals.value = StateMeals().copy(isLoading = true)
+                            }
+                        }
+                    }
+        }
+
+
+    }
 
     private fun getCategories() {
 
         viewModelScope.launch {
 
             container.getCategoriesUseCase()
-                .collectLatest {
+                    .collectLatest {
 
-                    result ->
+                        result ->
 
-                    when (result) {
+                        when (result) {
 
-                        is Resource.Loading -> {
+                            is Resource.Loading -> {
 
-                            _stateCategories.value =
-                                StateCategories().copy(isLoading = result.loading)
-                        }
-                        is Resource.Error -> {
-                            val errorMessage = result.errorMessage ?: "Unknown Error"
-                            _uiEvent.emit(UiEvent.ShowSnackbar(errorMessage))
-                        }
-                        is Resource.Success -> {
+                                _stateCategories.value =
+                                    StateCategories().copy(isLoading = result.loading)
+                            }
+                            is Resource.Error -> {
+                                val errorMessage = result.errorMessage ?: "Unknown Error"
+                                _uiEvent.emit(UiEvent.ShowSnackbar(errorMessage))
+                            }
+                            is Resource.Success -> {
 
-                            result.data?.let {
-                                _stateCategories.value = StateCategories().copy(categories = it)
+                                result.data?.let {
+                                    _stateCategories.value = StateCategories().copy(categories = it)
+                                }
                             }
                         }
                     }
-                }
         }
     }
 }
--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/states/StateMeals.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/states/StateMeals.kt
@@ -5,6 +5,7 @@ import com.uxstate.yummies.domain.model.Meal
 data class StateMeals(
     val searchQuery: String = "",
     val isLoading: Boolean = false,
-    val meals: List<Meal> = emptyList()
+    val meals: List<Meal> = emptyList(),
+    val fetchFromRemote:Boolean = false
 ) {
 }
