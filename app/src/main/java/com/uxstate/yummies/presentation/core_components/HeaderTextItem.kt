c82d6342a41f637c0ea85014185f2e62cc001e9f
Tonnie <vontonnie@gmail.com>
2022-10-24 18:15:45 +0300

    Add HeaderTextItem composable shell

---
+++ app/src/main/java/com/uxstate/yummies/presentation/core_components/HeaderTextItem.kt
@@ -0,0 +1,23 @@
+package com.uxstate.yummies.presentation.core_components
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+
+@Composable
+fun HeaderTextItem(
+    text: String,
+    modifier: Modifier = Modifier,
+    textSize: TextUnit = MaterialTheme.typography.body1.fontSize,
+    textAlignment: TextAlign = TextAlign.Start,
+    color: Color = MaterialTheme.colors.onSurface,
+    fontWeight: FontWeight = FontWeight.Bold,
+
+) {
+
+
+}
