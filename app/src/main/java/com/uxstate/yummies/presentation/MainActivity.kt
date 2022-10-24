772297279ccea37842bed199624da3563fe306bd
Tonnie <vontonnie@gmail.com>
2022-10-24 11:27:46 +0300

    Remove unused code from the MainActivity

--- app/src/main/java/com/uxstate/yummies/presentation/MainActivity.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/MainActivity.kt
@@ -23,22 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    YummiesTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/OverviewScreen.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/OverviewScreen.kt
@@ -1,9 +1,16 @@
 package com.uxstate.yummies.presentation.screens.overview_screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.uxstate.yummies.util.LocalSpacing
 
 @RootNavGraph(start = true)
 @Destination
@@ -11,6 +18,16 @@ import com.ramcosta.composedestinations.annotation.RootNavGraph
 @Composable
 fun OverviewScreen(viewModel: OverviewViewModel = hiltViewModel()) {
 
-    
+
+    val meals by viewModel.stateMeals.collectAsState()
+    val categories by viewModel.stateCategory.collectAsState()
+    val spacing = LocalSpacing.current
+
+Column(modifier = Modifier.fillMaxSize().padding(spacing.spaceSmall)) {
+
+
+
+}
+
 
 }
