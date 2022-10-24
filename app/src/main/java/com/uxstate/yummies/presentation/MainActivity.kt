3f605aacb9345e3cd71f716e3a6934e38c268d12
Tonnie <vontonnie@gmail.com>
2022-10-24 11:28:16 +0300

    Mark MainActivity with @AndroidEntryPoint

--- app/src/main/java/com/uxstate/yummies/presentation/MainActivity.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.yummies.presentation.ui.theme.YummiesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                
+
                 }
             }
         }
