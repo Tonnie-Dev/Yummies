2f280d4dc41fb66de599c9a20a79ff58bcad115f
Tonnie <vontonnie@gmail.com>
2022-10-24 09:05:59 +0300

    Add ic_category_error drawable

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/CategoryItem.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/CategoryItem.kt
@@ -1,13 +1,36 @@
 package com.uxstate.yummies.presentation.screens.overview_screen.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.uxstate.yummies.domain.model.Category
+import com.uxstate.yummies.util.LocalSpacing
 
 @Composable
 fun CategoryItem(category: Category, modifier: Modifier = Modifier, onClickCategory: () -> Unit) {
 
+    val spacing = LocalSpacing.current
+    Surface(
+            shape = RoundedCornerShape(spacing.spaceMedium),
+            elevation = spacing.spaceExtraSmall,
+            modifier = modifier.padding(spacing.spaceExtraSmall)
+    ) {
+
+        
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            
+            val painter = rememberAsyncImagePainter(model = ImageRequest.Builder().data().placeholder())
+            Image(painter = , contentDescription = )
+        }
+    }
 }
 
 
