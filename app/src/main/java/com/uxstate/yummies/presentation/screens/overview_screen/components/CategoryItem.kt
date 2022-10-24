bca3d735e9aa41ee304f99a3abfdaf7da5398fea
Tonnie <vontonnie@gmail.com>
2022-10-24 08:44:57 +0300

    Add CategoryItemPreview function

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/CategoryItem.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/CategoryItem.kt
@@ -2,9 +2,23 @@ package com.uxstate.yummies.presentation.screens.overview_screen.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.yummies.domain.model.Category
 
 @Composable
-fun CategoryItem(category: Category, modifier: Modifier = Modifier, onClickCategory: Category) {
+fun CategoryItem(category: Category, modifier: Modifier = Modifier, onClickCategory: () -> Unit) {
 
+}
+
+
+@Preview
+@Composable
+fun CategoryItemPreview() {
+
+    CategoryItem(category = Category(
+            categoryId = 13,
+            categoryType = "Mutton",
+            categoryDescription = "Mutton is Good",
+            categoryImageUrl = ""
+    ), onClickCategory = {})
 }
