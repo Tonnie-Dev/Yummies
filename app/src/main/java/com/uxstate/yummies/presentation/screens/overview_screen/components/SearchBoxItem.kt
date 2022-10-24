ae32b00a240b8a1eb13bc3eb13f86998a4a7b14a
Tonnie <vontonnie@gmail.com>
2022-10-24 11:01:30 +0300

    Add SearchBoxItemPreview function

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/SearchBoxItem.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/SearchBoxItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.yummies.util.LocalSpacing
 
 @Composable
@@ -20,4 +21,12 @@ fun SearchBoxItem(
     ) {
 
     }
+}
+
+
+@Preview
+@Composable
+fun SearchBoxItemPreview() {
+
+    SearchBoxItem(query = "text", onSearchTextChange = {})
 }
