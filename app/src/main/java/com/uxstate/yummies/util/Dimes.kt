0058ec717a27ec39273b7f726d4b1f21361bc1eb
Tonnie <vontonnie@gmail.com>
2022-10-24 08:39:39 +0300

    Add Dimens data class in the util package

---
+++ app/src/main/java/com/uxstate/yummies/util/Dimes.kt
@@ -0,0 +1,22 @@
+package com.uxstate.yummies.util
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+//data class with the default values
+data class Dimens(
+    val spaceDefault: Dp = 0.dp,
+    val spaceExtraSmall: Dp = 4.dp,
+    val spaceSmall: Dp = 8.dp,
+    val spaceMedium: Dp = 16.dp,
+    val spaceLarge: Dp = 32.dp,
+    val spaceExtraLarge: Dp = 64.dp,
+    val spaceOneHundred: Dp = 100.dp,
+    val spaceOneHundredFifty: Dp = 150.dp,
+    val spaceTwoHundred: Dp = 200.dp
+
+)
+
+
+val LocalSpacing = compositionLocalOf { Dimens() }
