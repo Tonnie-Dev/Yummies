1a1f47bad2afe421d9c5860081f627592d009786
Tonnie <vontonnie@gmail.com>
2022-10-24 08:42:12 +0300

    Define CategoryItem composable function

---
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/CategoryItem.kt
@@ -0,0 +1,10 @@
+package com.uxstate.yummies.presentation.screens.overview_screen.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.uxstate.yummies.domain.model.Category
+
+@Composable
+fun CategoryItem(category: Category, modifier: Modifier = Modifier, onClickCategory: Category) {
+
+}
