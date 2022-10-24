147eadb58e7d032e1c522f26baf127c99f0d8b7f
Tonnie <vontonnie@gmail.com>
2022-10-24 11:22:14 +0300

    Add OverviewScreen function shell

---
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/OverviewScreen.kt
@@ -0,0 +1,16 @@
+package com.uxstate.yummies.presentation.screens.overview_screen
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+
+@RootNavGraph(start = true)
+@Destination
+
+@Composable
+fun OverviewScreen(viewModel: OverviewViewModel = hiltViewModel()) {
+
+    
+
+}
