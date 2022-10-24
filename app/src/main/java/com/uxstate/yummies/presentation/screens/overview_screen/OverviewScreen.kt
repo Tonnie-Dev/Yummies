ac38430a19823d62b1ce343e5be3f28827795567
Tonnie <vontonnie@gmail.com>
2022-10-24 11:56:08 +0300

    Add SearchBoxItem to OverviewScreen

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/OverviewScreen.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/OverviewScreen.kt
@@ -14,6 +14,8 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.uxstate.yummies.presentation.screens.overview_screen.components.SearchBoxItem
+import com.uxstate.yummies.presentation.screens.overview_screen.overview_events.OverviewEvent
 import com.uxstate.yummies.util.LocalSpacing
 
 @RootNavGraph(start = true)
@@ -35,10 +37,19 @@ fun OverviewScreen(
             .padding(spacing.spaceSmall)
     ) {
 
+        // Header
         Text(
             text = "Find Best Recipe For Cooking",
             fontWeight = FontWeight.Bold,
             style = MaterialTheme.typography.h3
         )
+
+        // Search Box
+        SearchBoxItem(query = meals.searchQuery, onSearchTextChange = {
+            viewModel.onEvent(
+                OverviewEvent.OnSearchQueryChange("")
+            )
+        }) {
+        }
     }
 }
