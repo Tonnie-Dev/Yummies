f677f478cada30da6925f429ab90c6161e3d27ff
Tonnie <vontonnie@gmail.com>
2022-10-25 05:56:00 +0300

    Appy improvements to the MealItem composable

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/MealItem.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/MealItem.kt
@@ -1,10 +1,7 @@
 package com.uxstate.yummies.presentation.screens.overview_screen.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,7 +14,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.net.toUri
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.uxstate.yummies.R
@@ -38,28 +34,29 @@ fun MealItem(meal: Meal, modifier: Modifier = Modifier, onClickCategory: () -> U
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
-                .size(width = spacing.spaceOneHundred, spacing.spaceOneHundredFifty)
                 .padding(spacing.spaceExtraSmall)
         ) {
-
 
             val painter = rememberAsyncImagePainter(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(meal.imageUrl)
+                    // .size(Size.ORIGINAL)
+                    .crossfade(true)
                     .placeholder(R.drawable.loading_animation)
-                    .error(R.drawable.ic_category_error)
+                    .error(R.drawable.ic_meal_error)
                     .build()
             )
             Image(
                 painter = painter,
                 contentDescription = stringResource(R.string.meal_category_label),
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.weight(.7f)
+                modifier = Modifier.fillMaxWidth()
+
             )
 
             Text(
                 text = meal.name,
-                modifier = Modifier.weight(.3f),
+
                 style = MaterialTheme.typography.subtitle2,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
