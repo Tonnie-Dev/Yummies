2dc319c551a500e6553d1931b20118c425777915
Tonnie <vontonnie@gmail.com>
2022-10-26 08:16:17 +0300

    Add getCategorySearchWordUseCase to UseCaseContainer

--- app/src/main/java/com/uxstate/yummies/domain/use_cases/UseCaseContainer.kt
+++ app/src/main/java/com/uxstate/yummies/domain/use_cases/UseCaseContainer.kt
@@ -2,5 +2,6 @@ package com.uxstate.yummies.domain.use_cases
 
 data class UseCaseContainer(
     val getMealsUseCase: GetMealsUseCase,
-    val getCategoriesUseCase: GetCategoriesUseCase
+    val getCategoriesUseCase: GetCategoriesUseCase,
+    val getCategorySearchWordUseCase: GetCategorySearchWordUseCase
 )
