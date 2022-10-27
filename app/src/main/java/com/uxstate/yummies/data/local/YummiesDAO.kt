c43cd7ca7116fdb7ca13a3a3efcc47448cc80af8
Tonnie <vontonnie@gmail.com>
2022-10-27 19:08:04 +0300

    Override getWordByCategory() repository impl function

--- app/src/main/java/com/uxstate/yummies/data/local/YummiesDAO.kt
+++ app/src/main/java/com/uxstate/yummies/data/local/YummiesDAO.kt
@@ -17,7 +17,7 @@ interface YummiesDAO {
     suspend fun insertCategories(categories: List<CategoryEntity>)
 
     @Query(
-        """
+            """
         SELECT * FROM meals_table
 WHERE 
 LOWER(name) LIKE '%' || LOWER (:query)|| '%'
@@ -38,5 +38,5 @@ LOWER(name) LIKE '%' || LOWER (:query)|| '%'
     @Query("SELECT * FROM meals_table WHERE id LIKE :category")
     fun getMealsByCategory(category: String): Flow<List<MealEntity>>
 
-    /*Cecilia is a Zombie*/
+
 }
