31aff4f23e80ce68552e551ac0c0923451e1a123
Tonnie <vontonnie@gmail.com>
2022-10-30 08:18:22 +0300

    Add CategoryToggleButton() preview function

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/CategoryToggleButton.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/CategoryToggleButton.kt
@@ -1,21 +1,55 @@
 package com.uxstate.yummies.presentation.screens.overview_screen.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.uxstate.yummies.R
+import com.uxstate.yummies.presentation.core_components.HeaderTextItem
 import com.uxstate.yummies.util.LocalSpacing
 
 @Composable
 fun CategoryToggleButton(modifier: Modifier = Modifier) {
 
     val spacing = LocalSpacing.current
+    var isShowCategories by remember { mutableStateOf(false) }
     Row(
-            modifier = modifier
-                    .fillMaxWidth()
-                    .padding(spacing.spaceSmall)
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(spacing.spaceSmall),
+
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
 
+        HeaderTextItem(text = stringResource(id = R.string.categories_header_text))
+        Button(onClick = { isShowCategories = !isShowCategories }) {
+
+            Icon(
+                imageVector = if (isShowCategories)
+                    Icons.Default.KeyboardArrowUp
+                else
+                    Icons.Default.KeyboardArrowDown,
+                contentDescription = if (isShowCategories)
+                    "Show Categories"
+                else
+                    "Hide Categories"
+
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun CategoryToggleButtonPrev() {
+    CategoryToggleButton()
 }
