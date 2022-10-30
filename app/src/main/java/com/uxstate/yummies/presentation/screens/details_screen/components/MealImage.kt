a7bdf78d7631e9d2faf4ca8f7096de6a2b9de6d7
Tonnie <vontonnie@gmail.com>
2022-10-30 10:52:28 +0300

    Add MealImagePreview function

--- app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/MealImage.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/MealImage.kt
@@ -1,12 +1,18 @@
 package com.uxstate.yummies.presentation.screens.details_screen.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import com.uxstate.yummies.R
 import com.uxstate.yummies.domain.model.Meal
 import com.uxstate.yummies.util.LocalSpacing
-import com.uxstate.yummies.R
 
 @Composable
 fun MealImage(meal: Meal) {
@@ -19,5 +25,31 @@ fun MealImage(meal: Meal) {
                     .error(R.drawable.ic_meal_error)
                     .placeholder(R.drawable.loading_animation)
                     .build()
+
     )
+
+    Image(
+            painter = painter,
+            contentDescription = meal.name,
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(3f / 2f)
+                    .padding(spacing.spaceSmall)
+    )
+}
+
+@Preview
+@Composable
+fun MealImagePreview() {
+    MealImage(meal = Meal(
+            id = 0,
+            name = "",
+            category = "",
+            origin = "",
+            directions = "",
+            imageUrl = "",
+            ingredients = listOf(),
+            units = listOf(),
+            isFavorite = false
+    ))
 }
