e303723ef0980b94b8f6d1b6f3a4a4fa5332c1df
Tonnie <vontonnie@gmail.com>
2022-10-30 10:23:19 +0300

    Add Destination Screen

---
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/DetailsScreen.kt
@@ -0,0 +1,11 @@
+package com.uxstate.yummies.presentation.screens.details_screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+
+@Destination
+@Composable
+fun DetailsScreen() {
+
+}
