d55e3b05c708f1be052dcac7ced311cc6ac33328
Tonnie <vontonnie@gmail.com>
2022-10-31 19:50:19 +0300

    Add IngredientsCard() composable function

---
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/IngredientCard.kt
@@ -0,0 +1,24 @@
+package com.uxstate.yummies.presentation.screens.details_screen.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.uxstate.yummies.domain.model.Meal
+
+@Composable
+fun IngredientCard(meal: Meal, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+
+        Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            
+        }
+
+    }
+}
