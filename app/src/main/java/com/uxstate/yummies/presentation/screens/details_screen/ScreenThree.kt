ddc06c0a38085c27fe1edff7ae9c24c5fd4aae96
Tonnie <vontonnie@gmail.com>
2022-10-31 07:32:08 +0300

    Add ScreenThree

---
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/ScreenThree.kt
@@ -0,0 +1,10 @@
+package com.uxstate.yummies.presentation.screens.details_screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.uxstate.yummies.domain.model.Meal
+
+@Destination
+@Composable
+fun ScreenThree(meal: Meal) {
+}
