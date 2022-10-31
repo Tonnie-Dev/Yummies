cf1209e9e964c12c955acd739b106405107ddbf4
Tonnie <vontonnie@gmail.com>
2022-10-31 20:54:34 +0300

    Add LazyColumn to DetailsScreen

--- app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/DetailsScreen.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/DetailsScreen.kt
@@ -1,22 +1,37 @@
 package com.uxstate.yummies.presentation.screens.details_screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
 import com.uxstate.yummies.domain.model.Meal
 import com.uxstate.yummies.presentation.screens.details_screen.components.ImageDetailsPanel
+import com.uxstate.yummies.presentation.screens.details_screen.components.IngredientCard
 import com.uxstate.yummies.presentation.screens.details_screen.components.MealImage
+import com.uxstate.yummies.util.LocalSpacing
 
 @Destination
 @Composable
 fun DetailsScreen(meal: Meal) {
 
+    val spacing = LocalSpacing.current
     Column {
 
-// Meal Image
+        // Meal Image
         MealImage(meal = meal)
 
-// Image Details
+        // Image Details
         ImageDetailsPanel(meal = meal, onStar = { /*TODO*/ })
+
+        // ingredient
+        LazyColumn(contentPadding = PaddingValues(spacing.spaceSmall)) {
+
+            itemsIndexed(meal.ingredients) { i, ing ->
+
+                IngredientCard(ingredient = ing, measure = meal.units[i])
+            }
+        }
     }
 }
--- app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/IngredientCard.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/IngredientCard.kt
@@ -1,9 +1,6 @@
 package com.uxstate.yummies.presentation.screens.details_screen.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -12,6 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.yummies.R
 import com.uxstate.yummies.util.LocalSpacing
@@ -27,15 +26,32 @@ fun IngredientCard(ingredient: String, measure: String, modifier: Modifier = Mod
                 .fillMaxWidth()
                 .padding(spacing.spaceSmall),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.spacedBy(spacing.spaceExtraLarge)
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_ing_item),
                 contentDescription = ingredient
             )
 
-            Text(text = ingredient, style = MaterialTheme.typography.body1)
-            Text(text = measure, style = MaterialTheme.typography.caption)
+            Text(
+                text = ingredient,
+                style = MaterialTheme.typography.body1,
+                textAlign = TextAlign.Start,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                modifier = Modifier
+                    .width(spacing.spaceOneHundredFifty)
+
+            )
+            Text(
+                text = measure, style = MaterialTheme.typography.caption,
+                textAlign = TextAlign.End,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                modifier = Modifier
+                    .width(spacing.spaceExtraLarge)
+
+            )
         }
     }
 }
