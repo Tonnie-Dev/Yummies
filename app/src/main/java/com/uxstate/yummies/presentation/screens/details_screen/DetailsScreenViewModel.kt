2e81fffa464a6691ca4986cab705b0fcf3d8ac8d
Tonnie <vontonnie@gmail.com>
2022-11-02 20:07:46 +0300

    Add onEvent() inside the DetailsScreenViewModel

--- app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/DetailsScreenViewModel.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/DetailsScreenViewModel.kt
@@ -1,10 +1,32 @@
 package com.uxstate.yummies.presentation.screens.details_screen
 
 import androidx.lifecycle.ViewModel
-import com.uxstate.yummies.domain.repository.YummiesRepository
+import androidx.lifecycle.viewModelScope
+import com.uxstate.yummies.domain.use_cases.UseCaseContainer
+import com.uxstate.yummies.presentation.screens.details_screen.details_event.DetailsScreenEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailsScreenViewModel  @Inject constructor(repository: YummiesRepository) : ViewModel() {
+class DetailsScreenViewModel @Inject constructor(private val container: UseCaseContainer) :
+    ViewModel() {
+
+
+    fun onEvent(event: DetailsScreenEvent) {
+
+        when (event) {
+
+            is DetailsScreenEvent.OnStarMeal -> {
+
+                viewModelScope.launch {
+
+                    container.updateStarUseCase(
+                            meal = event.meal,
+                            newStarStatus = !event.meal.isFavorite
+                    )
+                }
+            }
+        }
+    }
 }
