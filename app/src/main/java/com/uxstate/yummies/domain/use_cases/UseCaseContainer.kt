50312a8d97c15595f8e0d160f028946873fb955d
Tonnie <vontonnie@gmail.com>
2022-11-02 19:58:21 +0300

    Add updateStarUseCase property to UseCaseContainer

--- app/src/main/java/com/uxstate/yummies/domain/use_cases/UseCaseContainer.kt
+++ app/src/main/java/com/uxstate/yummies/domain/use_cases/UseCaseContainer.kt
@@ -3,5 +3,6 @@ package com.uxstate.yummies.domain.use_cases
 data class UseCaseContainer(
     val getMealsUseCase: GetMealsUseCase,
     val getCategoriesUseCase: GetCategoriesUseCase,
-    val getMealsByCategoryUseCase: GetMealsByCategoryUseCase
+    val getMealsByCategoryUseCase: GetMealsByCategoryUseCase,
+    val updateStarUseCase: UpdateStarUseCase
 )
