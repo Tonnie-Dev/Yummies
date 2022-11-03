c43bd53b13c160c312a3b3579e6fbb77f9a135cb
Tonnie <vontonnie@gmail.com>
2022-11-03 22:15:21 +0300

    Add CheckStarredStatusUseCase class

---
+++ app/src/main/java/com/uxstate/yummies/domain/use_cases/CheckStarredStatusUseCase.kt
@@ -0,0 +1,12 @@
+package com.uxstate.yummies.domain.use_cases
+
+import com.uxstate.yummies.domain.model.Meal
+import com.uxstate.yummies.domain.repository.YummiesRepository
+
+class CheckStarredStatusUseCase(private val repository: YummiesRepository) {
+
+    suspend operator fun invoke(meal:Meal):Boolean{
+
+        return repository.checkStarredStatus(meal)
+    }
+}
