db047e3a58bae134344c6cf20769cdc7deea69bb
Tonnie <vontonnie@gmail.com>
2022-11-03 08:10:31 +0300

    Add UnstarUseCase class

---
+++ app/src/main/java/com/uxstate/yummies/domain/use_cases/UnStarUseCase.kt
@@ -0,0 +1,12 @@
+package com.uxstate.yummies.domain.use_cases
+
+import com.uxstate.yummies.domain.model.Meal
+import com.uxstate.yummies.domain.repository.YummiesRepository
+
+class UnStarUseCase(private val repository: YummiesRepository) {
+
+    suspend operator fun invoke(meal:Meal){
+
+        repository.removeFromStarredMeals(meal)
+    }
+}
