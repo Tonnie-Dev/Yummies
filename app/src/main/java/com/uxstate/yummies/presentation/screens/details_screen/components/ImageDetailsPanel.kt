7066a905c329b7187fcb2daea5b31f338bd74b78
Tonnie <vontonnie@gmail.com>
2022-11-03 08:53:53 +0300

    Add if-else block for ImageDetailsPanel

--- app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/ImageDetailsPanel.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/ImageDetailsPanel.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,42 +14,79 @@ import com.uxstate.yummies.presentation.core_components.HeaderTextItem
 import com.uxstate.yummies.util.LocalSpacing
 
 @Composable
-fun ImageDetailsPanel(meal: Meal, onStar: () -> Unit, modifier: Modifier = Modifier) {
+fun ImageDetailsPanel(
+    meal: Meal,
+    onStar: () -> Unit,
+    unStar: () -> Unit,
+    modifier: Modifier = Modifier
+) {
 
     val spacing = LocalSpacing.current
+    var isStarred by remember { mutableStateOf(meal.isFavorite) }
+
     Card(modifier = modifier) {
 
         Row(
 
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth().padding(spacing.spaceSmall)
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(spacing.spaceSmall)
         ) {
 
             Column() {
                 HeaderTextItem(text = meal.name, style = MaterialTheme.typography.h4)
                 Text(
-                    text = meal.origin,
-                    style = MaterialTheme.typography.h6,
-                    color = MaterialTheme.colors.onSurface,
-                    modifier = Modifier.padding(
-                        spacing.spaceSmall
-                    )
+                        text = meal.origin,
+                        style = MaterialTheme.typography.h6,
+                        color = MaterialTheme.colors.onSurface,
+                        modifier = Modifier.padding(
+                                spacing.spaceSmall
+                        )
                 )
             }
 
-            IconButton(
-                onClick = onStar,
-                modifier = Modifier.padding(spacing.spaceExtraSmall)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Star,
-                    contentDescription = "",
-                    tint = if (meal.isFavorite) MaterialTheme.colors.primary
-                    else Color.Gray.copy(ContentAlpha.disabled),
-                    modifier = Modifier.size(spacing.spaceLarge + spacing.spaceMedium)
-                )
+            if (isStarred) {
+                IconButton(
+                        onClick = {
+                            onStar()
+                            isStarred = !isStarred
+
+                        },
+                        modifier = Modifier.padding(spacing.spaceExtraSmall)
+                ) {
+                    Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = "",
+                            tint = if (isStarred) MaterialTheme.colors.primary
+                            else Color.Gray.copy(ContentAlpha.disabled),
+                            modifier = Modifier.size(spacing.spaceLarge + spacing.spaceMedium)
+                    )
+                }
+            } else {
+
+                IconButton(
+                        onClick = {
+                            unStar()
+                            isStarred = !isStarred
+
+                        },
+                        modifier = Modifier.padding(spacing.spaceExtraSmall)
+                ) {
+                    Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = "",
+                            tint = if (isStarred) MaterialTheme.colors.primary
+                            else Color.Gray.copy(ContentAlpha.disabled),
+                            modifier = Modifier.size(spacing.spaceLarge + spacing.spaceMedium)
+                    )
+                }
+
+
             }
+
+
         }
     }
 }
@@ -58,17 +95,18 @@ fun ImageDetailsPanel(meal: Meal, onStar: () -> Unit, modifier: Modifier = Modif
 @Composable
 fun ImageDetailsPanelPreview() {
     ImageDetailsPanel(
-        meal = Meal(
-            id = 0,
-            name = "Chapoo Dododo",
-            category = "",
-            origin = "Jamaica",
-            directions = "",
-            imageUrl = "",
-            ingredients = listOf(),
-            units = listOf(),
-            isFavorite = false
-        ),
-        onStar = {}
+            meal = Meal(
+                    id = 0,
+                    name = "Chapoo Dododo",
+                    category = "",
+                    origin = "Jamaica",
+                    directions = "",
+                    imageUrl = "",
+                    ingredients = listOf(),
+                    units = listOf(),
+                    isFavorite = false
+            ),
+            onStar = {},
+            unStar = {}
     )
 }
