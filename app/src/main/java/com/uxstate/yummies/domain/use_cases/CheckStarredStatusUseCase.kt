51772fa9594653fdeb64222c32a5383b756e51ae
Tonnie <vontonnie@gmail.com>
2022-11-04 19:00:15 +0300

    Update CheckStarredStatusUseCase

--- app/src/main/java/com/uxstate/yummies/domain/use_cases/CheckStarredStatusUseCase.kt
+++ app/src/main/java/com/uxstate/yummies/domain/use_cases/CheckStarredStatusUseCase.kt
@@ -2,10 +2,12 @@ package com.uxstate.yummies.domain.use_cases
 
 import com.uxstate.yummies.domain.model.Meal
 import com.uxstate.yummies.domain.repository.YummiesRepository
+import kotlinx.coroutines.flow.Flow
+
 
 class CheckStarredStatusUseCase(private val repository: YummiesRepository) {
 
-    suspend operator fun invoke(meal: Meal): Boolean {
+    operator fun invoke(meal: Meal): Flow<Boolean> {
 
         return repository.checkStarredStatus(meal)
     }
