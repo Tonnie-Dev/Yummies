59aa948ccdb1b79a4dba061ac029d67892715466
Tonnie <vontonnie@gmail.com>
2022-11-06 15:30:59 +0300

    Move the TextField into a Card for the SearchBoxItem

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/SearchBoxItem.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/SearchBoxItem.kt
@@ -1,7 +1,8 @@
 package com.uxstate.yummies.presentation.screens.overview_screen.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -23,40 +24,49 @@ fun SearchBoxItem(
 
     val spacing = LocalSpacing.current
 
-    TextField(
-        value = query,
-        onValueChange = onSearchTextChange,
-        placeholder = {
-            Text(
-                text = stringResource(R.string.search_placeholder_text),
-                color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
-            )
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(spacing.spaceSmall),
+        elevation = spacing.spaceSmall,
+        shape = RoundedCornerShape(spacing.spaceSmall)
+    ) {
+
+        TextField(
+            value = query,
+            onValueChange = onSearchTextChange,
+            placeholder = {
+                Text(
+                    text = stringResource(R.string.search_placeholder_text),
+                    color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+                )
+            },
+            textStyle = MaterialTheme.typography.h6,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    tint = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled),
+                    contentDescription = stringResource(R.string.search_icon_label)
+                )
+            }, trailingIcon = {
+            IconButton(onClick = onClearText) {
+
+                Icon(
+                    imageVector = Icons.Default.Close,
+
+                    contentDescription = stringResource(R.string.clear_text_icon)
+                )
+            }
         },
-        textStyle = MaterialTheme.typography.h6,
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                tint = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled),
-                contentDescription = stringResource(R.string.search_icon_label)
+            singleLine = true, modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = MaterialTheme.colors.primary,
+                focusedIndicatorColor = MaterialTheme.colors.background,
+                unfocusedIndicatorColor = MaterialTheme.colors.background,
+                textColor = MaterialTheme.colors.onSurface
             )
-        }, trailingIcon = {
-        IconButton(onClick = onClearText) {
-
-            Icon(
-                imageVector = Icons.Default.Close,
-
-                contentDescription = stringResource(R.string.clear_text_icon)
-            )
-        }
-    },
-        singleLine = true, modifier = Modifier.fillMaxWidth(),
-        colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = MaterialTheme.colors.surface,
-            focusedIndicatorColor = MaterialTheme.colors.background,
-            unfocusedIndicatorColor = MaterialTheme.colors.background,
-            textColor = MaterialTheme.colors.onSurface
         )
-    )
+    }
 }
 
 @Preview
