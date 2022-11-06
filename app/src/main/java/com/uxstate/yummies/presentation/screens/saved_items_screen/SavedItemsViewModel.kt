925351c06c8a86c75e8f4983e6dee1b059aa2e03
Tonnie <vontonnie@gmail.com>
2022-11-06 16:23:59 +0300

    Add getSavedMeals() in SavedItemsViewModel

--- app/src/main/java/com/uxstate/yummies/presentation/screens/saved_items_screen/SavedItemsViewModel.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/saved_items_screen/SavedItemsViewModel.kt
@@ -1,8 +1,15 @@
 package com.uxstate.yummies.presentation.screens.saved_items_screen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.uxstate.yummies.domain.model.Meal
 import com.uxstate.yummies.domain.use_cases.UseCaseContainer
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -10,6 +17,21 @@ import javax.inject.Inject
 class SavedItemsViewModel @Inject constructor(
     private val container: UseCaseContainer
 ) : ViewModel() {
-        
+
+    var savedMeals by mutableStateOf<List<Meal>>(emptyList())
+    private set
+
+    private fun getSavedMeals() {
+
+        viewModelScope.launch {
+            container.getStarredMeals()
+                    .collectLatest {
+                        savedMeals = it
+                    }
+
+
+        }
+
+    }
 
 }
