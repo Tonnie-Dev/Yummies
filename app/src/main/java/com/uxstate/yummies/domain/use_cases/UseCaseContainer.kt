59cb3b7eec70869f94fcb176f3dd3dfe9bfc8040
Tonnie <vontonnie@gmail.com>
2022-11-06 15:09:43 +0300

    Remove updateStarUseCase form the use case container

--- app/src/main/java/com/uxstate/yummies/domain/use_cases/UseCaseContainer.kt
+++ app/src/main/java/com/uxstate/yummies/domain/use_cases/UseCaseContainer.kt
@@ -4,7 +4,6 @@ data class UseCaseContainer(
     val getMealsUseCase: GetMealsUseCase,
     val getCategoriesUseCase: GetCategoriesUseCase,
     val getMealsByCategoryUseCase: GetMealsByCategoryUseCase,
-    val updateStarUseCase: UpdateStarUseCase,
     val starUseCase: StarUseCase,
     val unStarUseCase: UnStarUseCase,
     val checkStarredStatusUseCase: CheckStarredStatusUseCase,
