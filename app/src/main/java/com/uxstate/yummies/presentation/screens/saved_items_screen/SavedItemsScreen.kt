870f5d8623e04af38d6ba13e891e67828797e6b8
Tonnie <vontonnie@gmail.com>
2022-11-06 18:34:43 +0300

    Add Navigation back arrow to SavedItemsScreen

--- app/src/main/java/com/uxstate/yummies/presentation/screens/saved_items_screen/SavedItemsScreen.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/saved_items_screen/SavedItemsScreen.kt
@@ -3,13 +3,16 @@ package com.uxstate.yummies.presentation.screens.saved_items_screen
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -19,6 +22,7 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.yummies.R
 import com.uxstate.yummies.presentation.core_components.HeaderTextItem
 import com.uxstate.yummies.presentation.screens.saved_items_screen.components.MealBoxItem
+import com.uxstate.yummies.util.LocalSpacing
 
 @Destination
 @Composable
@@ -28,21 +32,26 @@ fun SavedItemsScreen(
 ) {
 
     val savedMeals = viewModel.savedMeals
+    val spacing = LocalSpacing.current
 
     Scaffold(
         topBar = {
-            Row() {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = stringResource(
-                        id = R.string.back_label
-                    ),
-                    tint = Color.White,
-                    modifier = Modifier.clickable {
-                        navigator.navigateUp()
-                    }
-                )
-                HeaderTextItem(text = "Saved Meals")
+            TopAppBar {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = stringResource(
+                            id = R.string.back_label
+                        ),
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .clickable {
+                                navigator.navigateUp()
+                            }
+                            .size(spacing.spaceLarge)
+                    )
+                    HeaderTextItem(text = "Saved Meals")
+                }
             }
         }
 
