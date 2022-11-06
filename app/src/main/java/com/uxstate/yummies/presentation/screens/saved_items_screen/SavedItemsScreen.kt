9d805b5c8e67d20c7233dbd033ecffcd9d343e53
Tonnie <vontonnie@gmail.com>
2022-11-06 16:41:40 +0300

    Add SavedItemsScreen composable

---
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/saved_items_screen/SavedItemsScreen.kt
@@ -0,0 +1,10 @@
+package com.uxstate.yummies.presentation.screens.saved_items_screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun SavedItemsScreen() {
+
+}
