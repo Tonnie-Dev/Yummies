748f10f3802b9df75ccb3168340959fb0e223630
Tonnie <vontonnie@gmail.com>
2024-08-02 19:50:27 +0300

    Update MealCard.kt

--- app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/MealCard.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/overview_screen/components/MealCard.kt
@@ -4,14 +4,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -40,96 +51,96 @@ fun MealCard(
     val spacing = LocalSpacing.current
 
     Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .clickable { onClickMeal(meal) }
-            .padding(spacing.spaceSmall),
+            modifier = modifier
+                    .fillMaxWidth()
+                    .clickable { onClickMeal(meal) }
+                    .padding(spacing.spaceSmall),
 
-        elevation = spacing.spaceSmall,
-        shape = RoundedCornerShape(spacing.spaceSmall)
+            elevation = CardDefaults.cardElevation(spacing.spaceSmall),
+            shape = RoundedCornerShape(spacing.spaceSmall)
     ) {
 
         Row(
-            modifier = Modifier.background(
-                brush = Brush.linearGradient(
-                    colors = MaterialTheme.colors.gradientColors
+                modifier = Modifier.background(
+                        brush = Brush.linearGradient(
+                                colors = MaterialTheme.colorScheme.gradientColors
+                        )
                 )
-            )
         ) {
 
             Column(
-                modifier = Modifier
-                    .weight(4.5f)
-                    .padding(spacing.spaceMedium),
-                verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                            .weight(4.5f)
+                            .padding(spacing.spaceMedium),
+                    verticalArrangement = Arrangement.Center,
 
-            ) {
+                    ) {
                 Text(
-                    text = meal.name,
-                    fontWeight = FontWeight.Bold,
-                    style = MaterialTheme.typography.h6,
-                    maxLines = 2,
-                    color = MaterialTheme.colors.onSurface
+                        text = meal.name,
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.headlineSmall,
+                        maxLines = 2,
+                        color = MaterialTheme.colorScheme.onSurface
                 )
 
-                Row() {
+                Row {
                     Icon(
-                        painter = painterResource(id = R.drawable.ic_flag),
-                        tint = Color.Black.copy(alpha = ContentAlpha.medium),
-                        contentDescription = stringResource(R.string.origin_text_label)
+                            painter = painterResource(id = R.drawable.ic_flag),
+                            tint = Color.Black.copy(alpha = .5f),
+                            contentDescription = stringResource(R.string.origin_text_label)
                     )
                     Text(
-                        text = meal.origin,
-                        style = MaterialTheme.typography.body1,
-                        color = MaterialTheme.colors.onSurface
+                            text = meal.origin,
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onSurface
                     )
                 }
 
                 Row(
-                    modifier = Modifier
-                        .border(
-                            width = spacing.spaceSingleDp,
-                            color = Color.Black,
-                            shape = CutCornerShape(spacing.spaceSmall)
-                        )
-                        .padding(spacing.spaceExtraSmall)
+                        modifier = Modifier
+                                .border(
+                                        width = spacing.spaceSingleDp,
+                                        color = Color.Black,
+                                        shape = CutCornerShape(spacing.spaceSmall)
+                                )
+                                .padding(spacing.spaceExtraSmall)
                 ) {
                     Text(
-                        text = "${meal.ingredientsCount} Ingredients",
-                        style = MaterialTheme.typography.caption,
-                        color = MaterialTheme.colors.onSurface,
-                        modifier = Modifier.padding(spacing.spaceExtraSmall)
+                            text = "${meal.ingredientsCount} Ingredients",
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.padding(spacing.spaceExtraSmall)
                     )
                 }
 
                 Spacer(modifier = Modifier.height(spacing.spaceSmall))
 
                 Icon(
-                    imageVector = Icons.Default.Star,
-                    contentDescription = "",
-                    tint = if (isStarred) MaterialTheme.colors.starredStarColor
-                    else Color.Gray.copy(ContentAlpha.disabled),
-                    modifier = Modifier.size(spacing.spaceLarge)
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "",
+                        tint = if (isStarred) MaterialTheme.colorScheme.starredStarColor
+                        else Color.Gray.copy(.3f),
+                        modifier = Modifier.size(spacing.spaceLarge)
                 )
             }
 
             val painter = rememberAsyncImagePainter(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(meal.imageUrl)
-                    // .size(Size.ORIGINAL)
-                    .crossfade(true)
-                    .placeholder(R.drawable.loading_animation)
-                    .error(R.drawable.ic_meal_error)
-                    .build()
+                    model = ImageRequest.Builder(LocalContext.current)
+                            .data(meal.imageUrl)
+                            // .size(Size.ORIGINAL)
+                            .crossfade(true)
+                            .placeholder(R.drawable.loading_animation)
+                            .error(R.drawable.ic_meal_error)
+                            .build()
             )
             Image(
-                painter = painter,
-                contentDescription = stringResource(R.string.meal_category_label),
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(spacing.spaceOneHundredFifty)
-                    .weight(5.5f)
-                    .clip(CircleShape)
+                    painter = painter,
+                    contentDescription = stringResource(R.string.meal_category_label),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                            .size(spacing.spaceOneHundredFifty)
+                            .weight(5.5f)
+                            .clip(CircleShape)
 
             )
         }
@@ -140,19 +151,19 @@ fun MealCard(
 @Composable
 fun MealCardPreview() {
     MealCard(
-        meal = Meal(
-            id = 0,
-            name = "Mexican Junk",
-            category = "",
-            origin = "",
-            directions = "",
-            imageUrl = "",
-            ingredients = listOf("A", "B", "C"),
-            units = listOf()
-        ),
-        onClickMeal = { /*TODO*/ },
+            meal = Meal(
+                    id = 0,
+                    name = "Mexican Junk",
+                    category = "",
+                    origin = "",
+                    directions = "",
+                    imageUrl = "",
+                    ingredients = listOf("A", "B", "C"),
+                    units = listOf()
+            ),
+            onClickMeal = { /*TODO*/ },
 
-        isStarred = false
+            isStarred = false
 
     )
 }
