1a8596a1e341b300c646b374b85ec6cb9078870e
Tonnie <vontonnie@gmail.com>
2024-08-02 08:07:50 +0300

    Update ImageDetailsPanel.kt

--- app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/ImageDetailsPanel.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/screens/details_screen/components/ImageDetailsPanel.kt
@@ -5,9 +5,14 @@ import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+
+
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,51 +39,52 @@ fun ImageDetailsPanel(
 
         Row(
 
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(spacing.spaceSmall)
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(spacing.spaceSmall)
         ) {
 
             Column(Modifier.weight(8.5f)) {
-                HeaderTextItem(text = meal.name, style = MaterialTheme.typography.h5)
+                HeaderTextItem(text = meal.name, style = MaterialTheme.typography.titleLarge)
+
                 Text(
-                    text = meal.origin,
-                    style = MaterialTheme.typography.h6,
-                    color = MaterialTheme.colors.onSurface,
-                    modifier = Modifier.padding(
-                        spacing.spaceSmall
-                    )
+                        text = meal.origin,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.padding(
+                                spacing.spaceSmall
+                        )
                 )
             }
 
             Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .weight(1.5f)
-                    .animateContentSize(
-                        animationSpec = tween(
-                            durationMillis = 300,
-                            easing = LinearOutSlowInEasing
-                        )
-                    )
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                            .weight(1.5f)
+                            .animateContentSize(
+                                    animationSpec = tween(
+                                            durationMillis = 300,
+                                            easing = LinearOutSlowInEasing
+                                    )
+                            )
             ) {
                 if (isMealStarred) {
 
                     // larger icon
                     Icon(
-                        imageVector = Icons.Default.Star,
-                        contentDescription = "",
-                        tint = MaterialTheme.colors.starredStarColor,
-                        modifier = Modifier
-                            .size(spacing.spaceExtraLarge + spacing.spaceSmall)
-                            .clickable {
+                            imageVector = Icons.Default.Star,
+                            contentDescription = "",
+                           tint = MaterialTheme.colorScheme.starredStarColor,
+                            modifier = Modifier
+                                    .size(spacing.spaceExtraLarge + spacing.spaceSmall)
+                                    .clickable {
 
-                                unStar()
-                                isMealStarred = !isMealStarred
-                            }
+                                        unStar()
+                                        isMealStarred = !isMealStarred
+                                    }
 
                     )
                 } else {
@@ -86,14 +92,14 @@ fun ImageDetailsPanel(
                     Icon(
                         imageVector = Icons.Default.Star,
                         contentDescription = "",
-                        tint = Color.Gray.copy(ContentAlpha.disabled),
+                        tint = Color.Gray.copy(alpha = 0.38f),
                         modifier = Modifier
                             .size(spacing.spaceLarge + spacing.spaceMedium + spacing.spaceSmall)
                             .clickable {
 
-                                onStar()
-                                isMealStarred = !isMealStarred
-                            }
+                                        onStar()
+                                        isMealStarred = !isMealStarred
+                                    }
                     )
                 }
             }
@@ -105,18 +111,18 @@ fun ImageDetailsPanel(
 @Composable
 fun ImageDetailsPanelPreview() {
     ImageDetailsPanel(
-        meal = Meal(
-            id = 0,
-            name = "Chapoo Dododo",
-            category = "",
-            origin = "Jamaica",
-            directions = "",
-            imageUrl = "",
-            ingredients = listOf(),
-            units = listOf()
-        ),
-        onStar = {},
-        unStar = {},
-        isStarred = false
+            meal = Meal(
+                    id = 0,
+                    name = "Chapoo Dododo",
+                    category = "",
+                    origin = "Jamaica",
+                    directions = "",
+                    imageUrl = "",
+                    ingredients = listOf(),
+                    units = listOf()
+            ),
+            onStar = {},
+            unStar = {},
+            isStarred = false
     )
 }
