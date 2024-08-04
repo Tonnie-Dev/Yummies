d9f39de82437c4ff9084dbf71bdf92beddd1f0e4
Tonnie <vontonnie@gmail.com>
2024-08-04 10:47:57 +0300

    Change from Cutlery to Spoon Icon

--- app/src/main/java/com/uxstate/yummies/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/uxstate/yummies/presentation/ui/theme/Color.kt
@@ -1,7 +1,6 @@
 package com.uxstate.yummies.presentation.ui.theme
 
 import androidx.compose.material3.ColorScheme
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
 
